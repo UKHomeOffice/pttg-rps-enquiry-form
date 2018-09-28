ddaa5febb8a56124d73654593c5b34e8ec87e292
Guy Sidebottom <guy.sidebottom@digital.homeoffice.gov.uk>
2018-09-28 08:20:50 +0100

    fix acceptance tests

--- apps/pttg-rps-enquiry-form/acceptance/features/factsheets.js
+++ apps/pttg-rps-enquiry-form/acceptance/features/factsheets.js
@@ -7,11 +7,11 @@ Before((I) => {
 const factsheets = [
     {option: 'What the EU Settlement Scheme is and who should apply',
         text: 'Settled status and the EU Settlement Scheme'},
-    {option: 'Application fees', text: 'Settled status and the EU Settlement Scheme'},
     {option: 'How to apply', text: 'How to apply'},
     {option: 'An application that’s already in progress', text: 'While your application is in progress'},
     {option: 'The result of my application', text: 'After you get a decision'},
-    {option: 'I’m from an employer, community group or other supporting organisation', text: null}
+    {option: 'I’m from an employer, community group or other supporting organisation',
+        text: 'Employers, community groups and supporting organisations'}
 ];
 
 factsheets.forEach((factsheet) => {
@@ -22,23 +22,33 @@ factsheets.forEach((factsheet) => {
         I.click('.link-back');
         I.seeInCurrentUrl(whatIsYourQuestionPage.url);
     });
-    if (factsheet.text) {
-        Scenario(`I click through the ${factsheet.option} factsheet`, (I, questionPage) => {
-            I.checkOption(factsheet.option);
-            I.submitForm();
-            I.click('This hasn’t answered my question');
-            I.seeInCurrentUrl(questionPage.url);
-        });
-        Scenario(`I get the ${factsheet.option} factsheet`, (I) => {
-            I.checkOption(factsheet.option);
-            I.submitForm();
-            I.see(factsheet.text);
-        });
-    } else {
-        Scenario(`I get the question page from ${factsheet.option}`, (I, questionPage) => {
-            I.checkOption(factsheet.option);
-            I.submitForm();
-            I.seeInCurrentUrl(questionPage.url);
-        });
-    }
+
+    Scenario(`I click through the ${factsheet.option} factsheet`, (I, questionPage) => {
+        I.checkOption(factsheet.option);
+        I.submitForm();
+        I.click('Go to enquiry form');
+        I.seeInCurrentUrl(questionPage.url);
+    });
+
+    Scenario(`I get the ${factsheet.option} factsheet`, (I) => {
+        I.checkOption(factsheet.option);
+        I.submitForm();
+        I.see(factsheet.text);
+    });
+
+    Scenario(`I get the question page from ${factsheet.option}`, (I, questionPage) => {
+        I.checkOption(factsheet.option);
+        I.submitForm();
+        I.see(factsheet.text);
+        I.submitForm();
+        I.seeInCurrentUrl(questionPage.url);
+    });
+});
+
+Scenario(`I get the question page from ${factsheets[4].option}`, (I, supportingOrgQuestionPage) => {
+    I.checkOption(factsheets[4].option);
+    I.submitForm();
+    I.see(factsheets[4].text);
+    I.submitForm();
+    I.seeInCurrentUrl(supportingOrgQuestionPage.url);
 });
---
+++ apps/pttg-rps-enquiry-form/acceptance/pages/supporting-org-question.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: 'supporting-org-question'
+};
