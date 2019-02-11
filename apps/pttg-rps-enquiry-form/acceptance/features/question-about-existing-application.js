e1eb294a54509dc01228ac8f2a3c93da0b41ea71
Owain Roberts <owain.roberts@bjss.com>
2019-02-11 14:51:16 +0000

    EE-14586 Added test to recreat issue where form data is kept if user clicks back button

--- apps/pttg-rps-enquiry-form/acceptance/features/question-about-existing-application.js
+++ apps/pttg-rps-enquiry-form/acceptance/features/question-about-existing-application.js
@@ -1,18 +1,35 @@
-Feature('Question About Existing Application Form');
-Before((I, existingApplicationQuestionPage) => {
-    I.amOnPage(existingApplicationQuestionPage.url);
+let assert = require('assert')
 
+Feature('Question About Existing Application Form');
+Before((I, startPage) => {
+    I.clearCookie();
+    I.amOnPage(startPage.url);
+    I.click('Start');
+    I.submitForm();
+    I.checkOption('What the EU Settlement Scheme is and who should apply');
+    I.submitForm();
+    I.checkOption('Yes');
+    I.submitForm();
 });
 
-Scenario('Form data not cached when back browser button pressed', (I, existingApplicationQuestionPage) => {
-    I.fillField('question-body', 'Test question');
-    I.fillField('applicant-email-address', 'test@test.com');
-    I.fillField('applicant-full-name', 'Test User');
-    I.fillField('application-number', '2418904');
+Scenario('Form data not cached when back browser button pressed',
+function *(I, existingApplicationQuestionPage) {
+    I.amOnPage(existingApplicationQuestionPage.url);
+    I.fillField('Your question', 'Test question');
+    I.fillField('Applicant’s email address', 'test@test.com');
+    I.fillField('Applicant’s full name', 'Test User');
+    I.fillField('Application number', '2418904');
     I.submitForm();
-    I.seeInCurrentUrl(confirmationPage.url);
-    
-    // I.checkOption('No');
-    // I.submitForm();
-    // I.seeInCurrentUrl(questionPage.url);
+
+    I.click('Back');
+    I.seeInCurrentUrl(existingApplicationQuestionPage.url);
+    I.click('Back');
+    // hit the browser back button
+    I.executeScript(() => {
+      window.history.back();
+    })
+    I.seeInCurrentUrl(existingApplicationQuestionPage.url);
+
+    let questionText = yield I.grabValueFrom('#question-body');
+    assert.equal(questionText, '');
 });
