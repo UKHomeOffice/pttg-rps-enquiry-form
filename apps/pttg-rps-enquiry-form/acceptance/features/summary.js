2442c1bce7d7ad3158f4726074107ac9ebd1d69f
Owain Roberts <owain.roberts@bjss.com>
2019-02-14 15:41:58 +0000

    EE-14585: improved test to work against link in leave page

--- apps/pttg-rps-enquiry-form/acceptance/features/summary.js
+++ apps/pttg-rps-enquiry-form/acceptance/features/summary.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Summary page');
 Before((I, startPage) => {
     I.clearCookie();
@@ -13,8 +15,21 @@ Before((I, startPage) => {
     I.submitForm();
 });
 
-Scenario('I can leave the summary page and clear my session without submitting the form', (I, leavePage) => {
+Scenario('I can leave the summary page and clear my session without submitting the form',
+  function *(I, leavePage) {
     I.see('Cancel and leave this service', 'button');
     I.click('Cancel and leave this service');
     I.seeInCurrentUrl(leavePage.url);
+
+    I.see('You have now left the service.');
+
+    let feedbackLink = yield I.grabAttributeFrom('#feedback', 'href');
+    let feedbackText = yield I.grabTextFrom('#feedback');
+    assert.equal(feedbackLink, 'https://www.gov.uk/done/eu-settled-status-enquiries');
+    assert.equal(feedbackText, 'What did you think of this service?');
+
+    let govUkHomePageLink = yield I.grabAttributeFrom('#returnToGovUk', 'href');
+    let govUkHomePageText = yield I.grabTextFrom('#returnToGovUk');
+    assert.equal(govUkHomePageLink, 'http://gov.uk/');
+    assert.equal(govUkHomePageText, 'Go back to GOV.UK');
 });
