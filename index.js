0b8124d1b694e1131a1d58f8037706189c53c747
Leigh Kirk <leighkirk@digital.homeoffice.gov.uk>
2019-02-14 09:19:54 +0000

    EE-12116 Remove logging from index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const hof = require('hof');
 const settings = require('./config');
 const notifyMessages = require('./apps/pttg-rps-enquiry-form/behaviours/notify-delivery-messages');
-const logger = require('./logger');
 
 settings.routes = settings.routes.map(route => require(route));
 settings.root = __dirname;
 settings.start = false;
 
 const app = hof(settings);
-
-logger.log({
-    level: 'info',
-    message: '***** Index.js *******'
-});
 
 //Endpoint for Notify delivery receipts
 app.use('/notify-messages', notifyMessages);
