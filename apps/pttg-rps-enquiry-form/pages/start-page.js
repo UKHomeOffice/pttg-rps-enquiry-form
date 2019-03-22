e60ec575a57e2f9fb674874fa353fdad137240c8
Leigh Kirk <leighkirk@digital.homeoffice.gov.uk>
2019-03-22 12:01:10 +0000

    Set local variable and check against to set the translation links on the start page only

--- apps/pttg-rps-enquiry-form/pages/start-page.js
+++ apps/pttg-rps-enquiry-form/pages/start-page.js
@@ -3,6 +3,7 @@ const WhatIsYourQuestionPage = require('./what-is-your-question');
 module.exports = {
     path: '/start',
     properties: {
+        behaviours: require('../behaviours/translation-link'),
         next: WhatIsYourQuestionPage.path
     }
 };
