71ac4089e4e272405320eda767ef57de3a4c3efe
Guy Sidebottom <guy.sidebottom@digital.homeoffice.gov.uk>
2020-06-03 14:14:49 +0100

    EE-32317: make acceptance tests work with additional fields, remove unneccessary fields

--- apps/pttg-rps-enquiry-form/acceptance/features/happy-path.js
+++ apps/pttg-rps-enquiry-form/acceptance/features/happy-path.js
@@ -20,13 +20,13 @@ xScenario('An enquiry can be completed end-to-end', (I) => {
 
 xScenario('An enquiry about an application can be completed end-to-end', (I) => {
     I.completeToStep('/confirm', {
-        'question-body': 'All human beings are born free and equal in dignity and rights.',
+        'question-body-existing-app': 'All human beings are born free and equal in dignity and rights.',
         'existing-application': 'yes',
         'application-number': '3434-0000-0000-0001',
         'applicant-email-address': 'test2@example.com',
         'applicant-full-name': 'Janine Butcher',
         'nationality': 'Sweden',
-        'phone-number': '+44-114496 0123',
+        'phone-number-existing-app': '+44-114496 0123',
         'date-of-birth-day': '01',
         'date-of-birth-month': '01',
         'date-of-birth-year': '1970'
