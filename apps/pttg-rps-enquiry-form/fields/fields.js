8be567f3894135195c7719a092f1798ce4b19963
Guy Sidebottom <guy.sidebottom@digital.homeoffice.gov.uk>
2020-06-05 16:03:39 +0100

    EE-32317: dry up the fields file by extracting duplicated objects out to variables

--- apps/pttg-rps-enquiry-form/fields/fields.js
+++ apps/pttg-rps-enquiry-form/fields/fields.js
@@ -1,183 +1,56 @@
+const emailTextBox = {
+    mixin: 'input-text',
+    validate: ['required', 'email', {
+        type: 'maxlength',
+        arguments: '254'
+    }]
+};
+
+const phoneTextBox = {
+    formatter: 'removespaces',
+    mixin: 'input-phone',
+    validate: 'phonenumber'
+};
+
+const applicationNumberTextBox = {
+    mixin: 'input-text',
+    validate: [
+        'required',
+        {
+            type: 'maxlength',
+            arguments: '25'
+        }
+    ],
+    child: '{{#t}}fields.application-number.example{{/t}}'
+};
+
+const questionBodyTextBox = {
+    mixin: 'textarea',
+    validate: [
+        'required', {
+            type: 'maxlength',
+            arguments: '2000'
+        }
+    ],
+    attributes: [{
+        attribute: 'rows',
+        value: 8
+    }]
+};
+
+const sharedTextBox = {
+    mixin: 'input-text',
+    validate: ['required', {
+        type: 'maxlength',
+        arguments: '255'
+    }]
+};
+
 module.exports = {
     'your-question-option': {
         mixin: 'radio-group',
         options: ['eligibility', 'how-to-apply', 'change-or-withdraw', 'application-result', 'supporting-organisation'],
         validate: 'required'
-    },
-    'your-email-address': {
-        mixin: 'input-text',
-        validate: ['required', 'email', {
-            type: 'maxlength',
-            arguments: '254'
-        }]
-    },
-    'your-email-address-supporting-org': {
-        mixin: 'input-text',
-        validate: ['required', 'email', {
-            type: 'maxlength',
-            arguments: '254'
-        }]
-    },
-    'applicant-email-address': {
-        mixin: 'input-text',
-        validate: ['required', 'email', {
-            type: 'maxlength',
-            arguments: '254'
-        }]
-    },
-    'applicant-email-address-supporting-org': {
-        mixin: 'input-text',
-        validate: ['required', 'email', {
-            type: 'maxlength',
-            arguments: '254'
-        }]
-    },
-    'nationality': {
-        mixin: 'select',
-        options: [{label: ' ', value: ''}].concat(require('hof-util-countries')())
-    },
-    'phone-number': {
-        formatter: 'removespaces',
-        mixin: 'input-phone',
-        validate: 'phonenumber'
-    },
-    'phone-number-supporting-org': {
-        formatter: 'removespaces',
-        mixin: 'input-phone',
-        validate: 'phonenumber'
-    },
-    'phone-number-supporting-org-existing-app': {
-        formatter: 'removespaces',
-        mixin: 'input-phone',
-        validate: 'phonenumber'
-    },
-    'phone-number-existing-app': {
-        formatter: 'removespaces',
-        mixin: 'input-phone',
-        validate: 'phonenumber'
-    },
-    'application-number': {
-        mixin: 'input-text',
-        validate: [
-            'required',
-            {
-                type: 'maxlength',
-                arguments: '25'
-            }
-        ],
-        child: '{{#t}}fields.application-number.example{{/t}}'
-    },
-    'application-number-supporting-org': {
-        mixin: 'input-text',
-        validate: [
-            'required',
-            {
-                type: 'maxlength',
-                arguments: '25'
-            }
-        ],
-        child: '{{#t}}fields.application-number.example{{/t}}'
-    },
-    'question-body': {
-        mixin: 'textarea',
-        validate: [
-            'required', {
-                type: 'maxlength',
-                arguments: '2000'
-            }
-        ],
-        attributes: [{
-            attribute: 'rows',
-            value: 8
-        }]
-    },
-    'question-body-existing-app': {
-        mixin: 'textarea',
-        validate: [
-            'required', {
-                type: 'maxlength',
-                arguments: '2000'
-            }
-        ],
-        attributes: [{
-            attribute: 'rows',
-            value: 8
-        }]
-    },
-    'question-body-supporting-org': {
-        mixin: 'textarea',
-        validate: [
-            'required', {
-                type: 'maxlength',
-                arguments: '2000'
-            }
-        ],
-        attributes: [{
-            attribute: 'rows',
-            value: 8
-        }]
-    },
-    'question-body-supporting-org-existing-app': {
-        mixin: 'textarea',
-        validate: [
-            'required', {
-                type: 'maxlength',
-                arguments: '2000'
-            }
-        ],
-        attributes: [{
-            attribute: 'rows',
-            value: 8
-        }]
-    },
-    'your-name': {
-        mixin: 'input-text',
-        validate: ['required', {
-            type: 'maxlength',
-            arguments: '255'
-        }]
-    },
-    'your-name-supporting-org': {
-        mixin: 'input-text',
-        validate: ['required', {
-            type: 'maxlength',
-            arguments: '255'
-        }]
-    },
-    'your-name-supporting-org-existing-app': {
-        mixin: 'input-text',
-        validate: ['required', {
-            type: 'maxlength',
-            arguments: '255'
-        }]
-    },
-    'organisation-name': {
-        mixin: 'input-text',
-        validate: ['required', {
-            type: 'maxlength',
-            arguments: '255'
-        }]
-    },
-    'organisation-name-existing-app': {
-        mixin: 'input-text',
-        validate: ['required', {
-            type: 'maxlength',
-            arguments: '255'
-        }]
-    },
-    'applicant-full-name': {
-        mixin: 'input-text',
-        validate: ['required', {
-            type: 'maxlength',
-            arguments: '255'
-        }]
-    },
-    'applicant-full-name-supporting-org': {
-        mixin: 'input-text',
-        validate: ['required', {
-            type: 'maxlength',
-            arguments: '255'
-        }]
     },
     'existing-application': {
         mixin: 'radio-group',
@@ -187,5 +60,26 @@ module.exports = {
         legend: {
             className: 'visually-hidden' // margin also changed in app.scss
         }
-    }
+    },
+    'your-email-address': emailTextBox,
+    'your-email-address-supporting-org': emailTextBox,
+    'applicant-email-address': emailTextBox,
+    'applicant-email-address-supporting-org': emailTextBox,
+    'phone-number': phoneTextBox,
+    'phone-number-supporting-org': phoneTextBox,
+    'phone-number-supporting-org-existing-app': phoneTextBox,
+    'phone-number-existing-app': phoneTextBox,
+    'application-number': applicationNumberTextBox,
+    'application-number-supporting-org': applicationNumberTextBox,
+    'question-body': questionBodyTextBox,
+    'question-body-existing-app': questionBodyTextBox,
+    'question-body-supporting-org': questionBodyTextBox,
+    'question-body-supporting-org-existing-app': questionBodyTextBox,
+    'your-name': sharedTextBox,
+    'your-name-supporting-org': sharedTextBox,
+    'your-name-supporting-org-existing-app': sharedTextBox,
+    'organisation-name': sharedTextBox,
+    'organisation-name-existing-app': sharedTextBox,
+    'applicant-full-name': sharedTextBox,
+    'applicant-full-name-supporting-org': sharedTextBox,
 };
